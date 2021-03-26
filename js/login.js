1e1acc2f43be25c8be441c521eecb6cf98a34524
GiseleCavalli <giselecavalli28@hotmail.com>
2021-03-25 22:56:38 -0300

    Responsividade telas e alterações pastas

--- js/login.js
+++ js/login.js
@@ -40,12 +40,8 @@ function verificarUsuario() {
 
 function verificarHASH(){
     var senha_hash_md5 = $.MD5($('#inputSenha').val());
-    
-    var myBitArray = sjcl.hash.sha256.hash($('#inputSenha').val());
-    var senha_hash_sha256 = sjcl.codec.hex.fromBits(myBitArray);
 
     alert(senha_hash_md5);
-    alert(senha_hash_sha256);
 
     return false;
 }
