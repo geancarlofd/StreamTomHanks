32a9b84d04b6e303df3ecae72012e3ccfae4464b
geancarlofd <geancarlofd@gmail.com>
2021-04-07 22:49:24 -0300

    Login

--- php/login.php
+++ php/login.php
@@ -5,9 +5,9 @@
 
     $con = mysqli_connect("localhost:3306", "root", "", "streamhanks"); /*Conectando com o banco*/
     
-    $result = mysqli_query($con, "SELECT * FROM usuario WHERE Email = '{$login}' AND Senha = '{$senha}' AND status = 1");
+    $result = mysqli_query($con, "SELECT * FROM usuario WHERE Email = '{$login}' AND Senha = '{$senha}' AND status = 1"); //resultado da query armazenando
 
-    $row = mysqli_num_rows($result);
+    $row = mysqli_num_rows($result);//armazenando rows
 
     if($row >= 1){
         echo json_encode("valido");
