edc586d3246882aacc505775eea2ac06e16b7034
GiseleCavalli <giselecavalli28@hotmail.com>
2021-04-21 23:21:11 -0300

    Rolagem carrossel

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -1,29 +1,61 @@
 
-window.onload = function (){
-    fLocalComunicaServidor("login_verifica");
-}
+const fila = document.querySelector('.conteudo-carrossel');
+const imagens = document.querySelectorAll('.imagens');
 
-function fLocalComunicaServidor(arquivo) {
+const flechaEsquerda = document.getElementById('flecha-esquerda');
+const flechaDireita = document.getElementById('flecha-direita');
 
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: "../../php/login/" + arquivo + ".php",
-        success: function (retorno) {
+/* Rolagem - Flecha direita */
+flechaDireita.addEventListener('click', () => {
+    fila.scrollLeft += fila.offsetWidth;
 
-            if (retorno.funcao == "cadastro") {
-                if (retorno.status == "s") {
-                    fLocalComunicaServidor("form-cadastrar", "envio_email");
-                    window.location.href = "../../index.html";
-                }
-                else {
-                    alert(retorno.mensagem);
-                    limparCampos();
-                    campos();
-                }
-            }
-        }
+    const indicadorAtivo = document.querySelector('.conteudo .ativo');
+    if(indicadorAtivo.nextSibling){
+        indicadorAtivo.nextSibling.classList.add('ativo');
+        indicadorAtivo.classList.remove('ativo');
+    }
+});
 
+/* Rolagem - Flecha esquerda */
+flechaEsquerda.addEventListener('click', () => {
+    fila.scrollLeft -= fila.offsetWidth;
+
+    const indicadorAtivo = document.querySelector('.conteudo .ativo');
+    if(indicadorAtivo.previousSibling){
+        indicadorAtivo.previousSibling.classList.add('ativo');
+        indicadorAtivo.classList.remove('ativo');
+    }
+});
+
+/* Paginação */
+const numeroPaginas = Math.ceil(imagens.length / 5);
+for(let i  = 0; i < numeroPaginas; i++){
+    const indicador = document.createElement('button');
+
+    if(i === 0){
+        indicador.classList.add('ativo');
+    }
+
+    document.querySelector('.conteudo').appendChild(indicador);
+    indicador.addEventListener('click', (e) => {
+        fila.scrollLeft = i * fila.offsetWidth;
+
+        document.querySelector('.conteudo .ativo').classList.remove('ativo');
+        e.target.classList.add('ativo');
     });
-
 }
+
+/* Hover */
+imagens.forEach((imagens) => {
+    peliculas.addEventListener('moussenter', (e) => {
+        const elemento  = e.currentTarget;
+        setTimeout(() => {
+            imagens.forEach(imagens  => imagens.classList.remove('hover'));
+            elemento.classList.add('hover');
+        }, 300);
+    });
+});
+
+fila.addEventListener('mouseleave', () => {
+    imagens.forEach(imagens  => imagens.classList.remove('hover'));
+});
